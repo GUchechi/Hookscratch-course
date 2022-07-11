6d30aa363707afb278a9b6130c550b398c52e998
Njoku Godswill Uchendu Uchechi <godswilluchendu7@gmail.com>
2022-07-11 11:00:51 -0700

    added my first useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [inputvalue, setInputValue] = useState('Uche');
+  let onChange = (event) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+     <input placeholder='enter sth...' onChange={onChange} />
+      {inputvalue}
     </div>
   );
 }
