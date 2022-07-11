6c11bc243c91e54f9c4933fe48571e0f43ce7e30
Njoku Godswill Uchendu Uchechi <godswilluchendu7@gmail.com>
2022-07-11 12:05:10 -0700

    added hoe to use "useReducer-hook"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import './App.css';
 
-function App() {
-  const [inputvalue, setInputValue] = useState('Uche');
-  let onChange = (event) => {
-    const newValue = event.target.value;
-    setInputValue(newValue);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT' :
+      return {count: state.count + 1, showText: state.showText}
+      case "toggleShowText" :
+        return {count: state.count, showText: !state.showText}
+        default : 
+            return state
   }
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer,
+     {count : 1, showText : true});
 
   return (
-    <div className="App">     
-     <input placeholder='enter sth...' onChange={onChange} />
-      {inputvalue}
+    <div className="App"> 
+    <h1>{state.count}</h1>    
+      <button
+      onClick={ () => {
+        dispatch({type: 'INCREMENT'});
+        dispatch({type: 'toggleShowText'});
+      }}>Click here</button>
+      {state.showText && <p>This is a text</p>}
     </div>
   );
 }
